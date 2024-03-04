f2ceeebe7714594967f2911969b2f6b78967146e
PatrykS <sawicki.patryk@gmail.com>
2024-03-04 20:42:39 +0100

    modifying greeter project - CMakeLists.txt

--- greeter/greeter.cpp
+++ greeter/greeter.cpp
@@ -1,29 +1,24 @@
 #include "greeter.hpp"
 #include <iostream>
 
-Greeter::Greeter()
-{
+Greeter::Greeter() {
     std::cout << "Hello handsome :)\n";
 }
 
-Greeter::Greeter(const std::string & name)
-    : name_(name)
-{
+Greeter::Greeter(const std::string& name)
+    : name_(name) {
     std::cout << "Hello " << name_ << "!\n";
 }
 
-Greeter::Greeter(const Person & person)
-    : name_(person.getName())
-{
+Greeter::Greeter(const Person& person)
+    : name_(person.getName()) {
     std::cout << "Hello " << name_ << "!\n";
 }
 
-Greeter::~Greeter()
-{
+Greeter::~Greeter() {
     std::cout << "Goodbye " << name_ << "!\n";
 }
 
-std::string Greeter::meet() const
-{
+std::string Greeter::meet() const {
     return "It's nice to meet you " + name_ + " :)\n";
 }
